1e038a37a3b45c2055c62cc4eaab9a0a932c9ffc
HiBixby <201910789@sangmyung.kr>
2023-02-18 15:32:54 +0900

    ✨ Todo 페이지 틀 잡기

--- src/views/Todo.tsx
+++ src/views/Todo.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Todo = () => {
-  return <>todo page</>;
+  return (
+    <>
+      <nav><button>뒤로</button></nav>
+      <time>2022.09.06</time>
+      <time>14:50</time>
+      <form>
+        <label>Todo</label>
+        <input type="text" placeholder="제목을 입력해주세요." />
+        <label>Link</label>
+        <input type="url" placeholder="URL을 입력해주세요." />
+      </form>
+    </>
+  );
 };
 
 export default Todo;
