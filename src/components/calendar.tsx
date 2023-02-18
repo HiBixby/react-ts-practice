5aad96f9bfaa2764931154d06760a02a915e4233
HiBixby <201910789@sangmyung.kr>
2023-02-18 15:32:29 +0900

    ♻️ 스타일드 컴포넌트를 객체화

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -3,45 +3,75 @@ import styled from "styled-components";
 import { ReactComponent as RightArrowIcon } from "../assets/btn-right-arrow.svg";
 import { ReactComponent as LeftArrowIcon } from "../assets/btn-left-arrow.svg";
 
-const CalendarLayout = styled.div`
-  width: 90.7%;
-  height: 35.3%;
-  border-radius: 10px;
-  background-color: white;
-  box-shadow: 0 3px 6px 0 var(--black-16);
-  overflow-x: scroll;
-`;
-const YearTime = styled.time`
-  margin: 7.2% 0 0 5.6%;
-  font-size: 1.125rem;
-  font-weight: 600;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: 1.17;
-  letter-spacing: normal;
-  display: block;
-  color: var(--green-blue);
-`;
+const S = {
+  CalendarLayout: styled.div`
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    background-color: white;
+    box-shadow: 0 3px 6px 0 var(--black-16);
+    overflow-x: scroll;
+  `,
+  YearTime: styled.time`
+    margin: 1.705rem 0 0 1.344rem;
+    font-size: 1.125rem;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.17;
+    letter-spacing: normal;
+    display: block;
+    color: var(--green-blue);
+  `,
 
-const MonthTime = styled.time`
-  margin: 0 0 0 5.4%;
-  font-size: 1.563rem;
-  font-weight: 900;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: 1.2;
-  letter-spacing: normal;
-  display: block;
-  color: var(--green-blue);
-`;
+  MonthTime: styled.time`
+    font-size: 1.563rem;
+    font-weight: 900;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.2;
+    letter-spacing: normal;
+    display: block;
+    color: var(--green-blue);
+  `,
+
+  SecondLineBox: styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 1.742rem 0 1.281rem;
+  `,
+
+  ArrowBtnBox: styled.div`
+    width: 2.313rem;
+    display: flex;
+    justify-content: space-between;
+  `,
+  ChangeMonthBtn: styled.button`
+    border: none;
+    background: transparent;
+    padding: 0;
+    &:hover {
+      cursor: pointer;
+    }
+  `,
+};
 const Calendar = () => {
   return (
-    <CalendarLayout>
-      <YearTime>2023</YearTime>
-      <MonthTime>02</MonthTime>
-      <LeftArrowIcon />
-      <RightArrowIcon />
-    </CalendarLayout>
+    <S.CalendarLayout>
+      <S.YearTime>2023</S.YearTime>
+      <S.SecondLineBox>
+        <S.MonthTime>02</S.MonthTime>
+        <S.ArrowBtnBox>
+          <S.ChangeMonthBtn title="이전 달">
+            <LeftArrowIcon />
+          </S.ChangeMonthBtn>
+          <S.ChangeMonthBtn title="다음 달">
+            <RightArrowIcon />
+          </S.ChangeMonthBtn>
+        </S.ArrowBtnBox>
+      </S.SecondLineBox>
+    </S.CalendarLayout>
   );
 };
 
